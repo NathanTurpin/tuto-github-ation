83fbe06064c9982138e030ae2e07759e5fd10a2b
turpinNathan <nathan.turpin@free.fr>
2023-05-24 14:22:45 +0200

    1er test fonctionnel

--- cypress/e2e/fizzbuzz.cy.js
+++ cypress/e2e/fizzbuzz.cy.js
@@ -1,6 +1,17 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("http://localhost:5173/");
-    cy.get("#title").contains("coucou");
+import { passwordValidator } from "@/utils/passwordValidator";
+
+describe("passwordValidator", () => {
+  it("retourne un booléen et un tableau", () => {
+    const { isValid, errors } = passwordValidator("");
+    expect(typeof isValid).toBe("boolean");
+    expect(Array.isArray(errors)).toBe(true);
+  });
+
+  it("valide la longueur minimale du mot de passe", () => {
+    const { isValid, errors } = passwordValidator("passe");
+    expect(isValid).toBe(false);
+    expect(errors).toContain(
+      "Le mot de passe doit comporter au moins 8 caractères"
+    );
   });
 });
