c3d505d7093684dcc68a3603bf1bc4ecd320e225
turpinNathan <nathan.turpin@free.fr>
2023-05-24 15:10:11 +0200

    test unitaire mdp > 8 car

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,5 +1,12 @@
-import { test,expect } from 'vitest'
+import { it,expect,describe } from 'vitest'
 
-test('basic arithmetic', () => {
-  expect(1 + 1).toEqual(2);
+import { passwordValidator } from "../src/utils/passwordValidator";
+
+describe('passwordValidator', () => {
+  it('retourne une erreur si le mot de passe a moins de 8 caractères', () => {
+    const { isValid, errors } = passwordValidator('pass12');
+
+    expect(isValid).toBe(false);
+    expect(errors).toContain('Le mot de passe doit comporter au moins 8 caractères');
+  });
 });
